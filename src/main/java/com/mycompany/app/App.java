fe5ab34a4a35c11ffa8fd1b202eb6a0a53afd342
DamPhonggg <160729284+DamPhonggg@users.noreply.github.com>
2024-02-28 10:39:43 +0000

    BT TH Week 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,32 +3,27 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
-package com.mycompany.app;
+ package com.mycompany.app;
 
-/*public class App {
-    public static void main(String[] args) {
-        int time = 22;
-if (time < 10) {
-  System.out.println("Good morning.");
-} else if (time < 18) {
-  System.out.println("Good day.");
-} else {
-  System.out.println("Good bro.");
-}
-    }
-}*/
-
- class Number {
-      public int i;
-}
-  class App {
-  public static void main(String[] args) {
-    Number n1 = new Number();
-    Number n2 = new Number();
-    n1.i = 2;
-    n2.i = 5;
-    n1.i = n2.i;
-    n2.i = 10//; 
-    System.out.println(n1.i);
-  }
-}
+ public class App {
+     static class Book {
+         String title;
+         String author;
+         int numPages;
+ 
+         Book() {
+         }; // default constructor
+ 
+         public Book(String t, String a, int p) {
+             title = t;
+             author = a;
+             numPages = p;
+         }
+     }
+ 
+     public static void main(String[] args) {
+         Book myObj = new Book("a", "b", 2); // New book
+         System.out.println(myObj.title);
+     }
+ }
+ 
