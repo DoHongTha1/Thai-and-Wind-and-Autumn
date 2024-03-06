4e8f5cb615f302245eeafc5d0554c36f28662581
Tha1 <160729218+DoHongTha1@users.noreply.github.com>
2024-03-06 10:07:10 +0000

    Excersice

---
+++ src/main/java/com/mycompany/app/week3/DisruptLecture.java
@@ -0,0 +1,31 @@
+package com.mycompany.app.week3;
+
+public class DisruptLecture {
+   public static class CellPhone {
+        CellPhone() {
+
+        }
+
+        public void ring(Tune t) {
+            t.play();
+        }
+    }
+
+    public static class Tune {
+        public void play() {
+            System.out.println("Tune.play()");
+        }
+    }
+
+    public static class ObnoxiousTune extends Tune {
+        ObnoxiousTune() {
+            } 
+    }
+
+        public static void main(String[] args) {
+            CellPhone noiseMaker = new CellPhone();
+            ObnoxiousTune ot = new ObnoxiousTune();
+            noiseMaker.ring(ot); // ot works though Tune called for
+        }
+    }
+
