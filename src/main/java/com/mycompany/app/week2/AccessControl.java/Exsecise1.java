fb5781daba10d3f39b818780eefba5186cdba36a
DamPhonggg <160729284+DamPhonggg@users.noreply.github.com>
2024-03-29 13:28:27 +0000

    Exercise1.java

---
+++ src/main/java/com/mycompany/app/week2/AccessControl.java/Exsecise1.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.Week2.Code2.AccessControl;
+
+/*Exercise 1: Create a class in a package. Create an instance of your class outside of that
+package */
+
+import com.mycompany.app.Week2.Code2.SourceAccessControl.MyClass;
+
+public class Exercise1 {
+    public static void AccessControlE1 () {
+        MyClass myObject = new MyClass(); // Create an instance of MyClass
+        myObject.myMethod(); // Call the method on the object
+    }
+}
